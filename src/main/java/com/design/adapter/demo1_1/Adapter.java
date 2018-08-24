37f5e9ca6a4e92cd7eeb04f71a3f943b5bff127f
JeeLearner <15501112566@163.com>
2018-08-24 10:15:39 +0800

    适配器模式

--- src/main/java/com/design/adapter/demo1_1/Adapter.java
+++ src/main/java/com/design/adapter/demo1_1/Adapter.java
@@ -10,7 +10,6 @@ public class Adapter extends Adaptee implements Target{
     //这里需要和被适配对象关联起来：1.继承   2.组合(推荐)
     private Adaptee adaptee;
 
-    @Override
     public void handleReq() {
         super.request();
     }
--- src/main/java/com/design/adapter/demo1_2/Adapter.java
+++ src/main/java/com/design/adapter/demo1_2/Adapter.java
@@ -10,7 +10,6 @@ public class Adapter implements Target{
     //这里需要和被适配对象关联起来：1.继承   2.组合(推荐)
     private Adaptee adaptee;
 
-    @Override
     public void handleReq() {
         adaptee.request();
     }
